689ec1f8fec0f7931ef41820f70f735f599512b5
Juan Buitrago <buitragomjuan@outlook.com>
2024-11-11 12:08:55 -0500

    puertos de aplicacion completos, con usecases

--- mongo-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mongo/repository/PersonaRepositoryMongo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.javeriana.as.personapp.mongo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import co.edu.javeriana.as.personapp.mongo.document.PersonaDocument;
-
-public interface PersonaRepositoryMongo extends MongoRepository<PersonaDocument, Integer> {
-
-}
